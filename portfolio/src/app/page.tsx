53b6ae859bc90b5f05b10d1481f0e1865ecc50f8
Jiaqi Fu(Jack) <fujf1@student.unimelb.edu.au>
2024-11-10 15:33:16 +1100

    Setup template

---
+++ portfolio/src/app/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const HomePage = () => {
+    return (
+        <div>
+            HomePage
+            <p>yes</p>
+        </div>
+    );
+};
+
+export default HomePage;
