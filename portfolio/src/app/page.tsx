cc6f9cfedcafcdea44101960a1c9364976aee228
Jiaqi Fu(Jack) <fujf1@student.unimelb.edu.au>
2024-11-11 22:33:01 +1100

    fixed css for hero section

--- portfolio/src/app/page.tsx
+++ portfolio/src/app/page.tsx
@@ -3,15 +3,10 @@ import HeroSection from "@/components/sections/HeroSection";
 
 const HomePage = () => {
     return (
-        <div>
-            <div className="container">
+        <main>
+            <HeroSection />
 
-                <HeroSection />
-            </div>
-            
-
-
-        </div>
+        </main>
     );
 };
 
--- portfolio/src/components/sections/HeroSection.tsx
+++ portfolio/src/components/sections/HeroSection.tsx
@@ -5,20 +5,20 @@ import Image from "next/image";
 import { ShootingStars } from "../ui/ShootingStars";
 import { StarsBackground } from "../ui/StarBackground";
 
-
-
 const HeroSection = () => {
-	
     return (
-        <div className="container">
-            <Spotlight
-                className="-top-30 left-0 md:left-60 md:-top-20 h-full"
-                fill="white"
-            />
-			<ShootingStars maxSpeed={5} maxDelay={5000} starWidth={20} starHeight={5} />
-			<StarsBackground />
-            <section className="h-full w-screen flex flex-col relative mx-auto">
-                <div className="w-full mx-auto flex flex-col mt-20 max-w-lg">
+        <div className="container relative z-0 mx-auto h-[90vh]">
+            <section className="h-full w-full flex flex-col mx-auto">
+                <div className="inline-flex mx-auto mt-10 ">
+                    <Image
+                        className="rounded-lg fade-in"
+                        src={avatar}
+                        width={100}
+                        height={100}
+                        alt="placeholder avatar"
+                    />
+                </div>
+                <div className="w-full mx-auto flex flex-col mt-5 max-w-lg">
                     <p className="sm:text-3xl text-xl font-medium text-white text-center fade-in">
                         Hi, I'm <span className="text-teal">Jack</span>
                         <span className="waving-hand">👋</span>
@@ -34,11 +34,25 @@ const HeroSection = () => {
                         applications and always eager to learn and grow in the
                         tech industry.
                     </p>
-                </div>
-				<div className="inline-flex mx-auto ">
-					<Image className="rounded-lg" src={avatar} width={100} height={100} alt="placeholder avatar" />
-				</div>
+                </div>                
+                <button className="text-gray-400">github</button>
+                <button className="text-gray-400">linkedin</button>
+                <button className="text-gray-400">download resume</button>
+                <button className="text-gray-400">download resume</button>
             </section>
+
+            <Spotlight
+                className="-top-30 left-0 md:left-60 md:-top-20 h-full"
+                fill="white"
+            />
+
+                <ShootingStars
+                    maxSpeed={5}
+                    maxDelay={5000}
+                    starWidth={20}
+                    starHeight={5}
+                />
+                <StarsBackground />
 
         </div>
     );
